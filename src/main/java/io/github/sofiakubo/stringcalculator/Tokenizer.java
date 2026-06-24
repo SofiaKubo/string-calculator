9a88b09a39e74e5e31064b84578572e5d9562df9
SofiaKubo <skuboreva@gmail.com>
2026-06-24 13:14:44 +0200

    feat: rewrite tokenize() to buffer multi-digit numbers (green)

--- src/main/java/io/github/sofiakubo/stringcalculator/Tokenizer.java
+++ src/main/java/io/github/sofiakubo/stringcalculator/Tokenizer.java
@@ -1,10 +1,27 @@
 package io.github.sofiakubo.stringcalculator;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Tokenizer {
 
     public List<String> tokenize(String expression) {
-        return List.of(expression.split(""));
+        StringBuilder buffer = new StringBuilder();
+        List<String> tokens = new ArrayList<>();
+
+        for (char c : expression.toCharArray()) {
+            if (Character.isDigit(c)) {
+                buffer.append(c);
+            } else {
+                tokens.add(buffer.toString());
+                buffer.setLength(0);
+                tokens.add(String.valueOf(c));
+            }
+        }
+
+        if (!buffer.isEmpty()) {
+            tokens.add(buffer.toString());
+        }
+        return tokens;
     }
 }
